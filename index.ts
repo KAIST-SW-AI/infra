e15ecbc5e258754fa93b3f90dfdff76fc727cb22
andyye <63383967+0ev@users.noreply.github.com>
2024-07-12 22:52:17 +0900

    test

--- index.ts
+++ index.ts
@@ -2,38 +2,29 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const chatgptBot = new aws.lex.Bot("chatgpt-bot", {
-  abortStatement: {
-    messages: [
-      {
-        content: "Sorry, I am not able to assist at this time",
-        contentType: "PlainText",
-      },
-    ],
+// Replace these variables with your actual values
+const connectInstanceId = "929671d3-1cef-4413-aa40-e23eee52e79b";
+
+// Create a Kinesis Stream
+const kinesisStream = new aws.kinesis.Stream("myKinesisStream", {
+  streamModeDetails: {
+    streamMode: "ON_DEMAND",
   },
-  childDirected: false,
-  clarificationPrompt: {
-    maxAttempts: 2,
-    messages: [
-      {
-        content: "I didn't understand you, what would you like to do?",
-        contentType: "PlainText",
-      },
-    ],
-  },
-  createVersion: false,
-  description: "Bot to order flowers on the behalf of a user",
-  idleSessionTtlInSeconds: 600,
-  intents: [
-    {
-      intentName: "OrderFlowers",
-      intentVersion: "1",
-    },
-  ],
-  locale: "en-US",
-  name: "OrderFlowers",
-  processBehavior: "BUILD",
-  voiceId: "Salli",
 });
 
-export const bot = chatgptBot.id;
+const instanceStorageConfig = new aws.connect.InstanceStorageConfig(
+  "instanceStorageConfig",
+  {
+    instanceId: connectInstanceId,
+    resourceType: "REAL_TIME_CONTACT_ANALYSIS_CHAT_SEGMENTS",
+    storageConfig: {
+      storageType: "KINESIS_STREAM",
+      kinesisStreamConfig: {
+        streamArn: kinesisStream.arn,
+      },
+    },
+  }
+);
+
+// Export the storage config ID
+export const storageConfigId = instanceStorageConfig.id;
